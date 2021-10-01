bc479feb5746fb5ce6087508e7c26ead37b14f73
unknown <nikhilownstheworld@gmail.com>
2021-10-01 11:58:21 +0530

    initial commit

---
+++ model.py
@@ -0,0 +1,11 @@
+class Model:
+    def __init__(self):
+        prog = {
+            'iden': {},
+            'env': {},
+            'data': {},
+            'proc': {}
+        }
+    
+    def build_model(self, code):
+        pass
